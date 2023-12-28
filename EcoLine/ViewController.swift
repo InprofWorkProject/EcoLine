10f57d70fe98bb6de69ed7b223eafd77ae6ecfb5
inprof <inprof@inprof-no-iMac.local>
2023-12-28 18:20:18 +0900

    Initial Commit

---
+++ EcoLine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EcoLine
+//
+//  Created by inprof on 2023/12/28.
+//  Copyright © 2023 Inprof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
