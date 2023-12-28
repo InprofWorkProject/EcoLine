8f6bbb7fdfa9b438c9a7cdb3f5085839b15b81db
inprof <inprof@inprof-no-iMac.local>
2023-12-29 07:44:16 +0900

    LineList Controller add

---
+++ EcoLine/ItemList_s_Controller.swift
@@ -0,0 +1,17 @@
+//
+//  ItemList_s_Controller.swift
+//  EcoLine
+//
+//  Created by inprof on 2023/12/29.
+//  Copyright © 2023 Inprof. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemListController : UIViewController{
+    @IBOutlet var ListTable: [UITableView]!
+    @IBOutlet weak var UpperNav: UINavigationBar!
+    @IBOutlet weak var LowerNav: UINavigationBar!
+    
+}
